05c24bc5e2dfc6da04af68647d5ce66b449a8f92
mohamed-Fathy1 <60184431+mohamed-Fathy1@users.noreply.github.com>
2023-12-27 04:35:10 +0000

    Update

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+import discord
+
+client = discord.Client(intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+  print(f"We have logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  if message.content.startswith("$hello"):
+    await message.channel.send("Hello!")
+
+client.run(os.environ['TOKEN'])
